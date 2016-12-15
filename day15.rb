1b3fb091c29865e008660e9444a90e6638bb89af
stain88 <marcbaghdadi@gmail.com>
2016-12-15 12:05:18 +0000

    day15 part 2

--- day15.rb
+++ day15.rb
@@ -1,9 +1,19 @@
+# Part 1
 input = "Disc #1 has 5 positions; at time=0, it is at position 2.
 Disc #2 has 13 positions; at time=0, it is at position 7.
 Disc #3 has 17 positions; at time=0, it is at position 10.
 Disc #4 has 3 positions; at time=0, it is at position 2.
 Disc #5 has 19 positions; at time=0, it is at position 9.
 Disc #6 has 7 positions; at time=0, it is at position 0."
+
+# Part 2
+input = "Disc #1 has 5 positions; at time=0, it is at position 2.
+Disc #2 has 13 positions; at time=0, it is at position 7.
+Disc #3 has 17 positions; at time=0, it is at position 10.
+Disc #4 has 3 positions; at time=0, it is at position 2.
+Disc #5 has 19 positions; at time=0, it is at position 9.
+Disc #6 has 7 positions; at time=0, it is at position 0.
+Disc #7 has 11 positions; at time=0, it is at position 0."
 
 discs = []
 
